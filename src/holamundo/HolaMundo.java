126a5fc95877ec7d332309ced09dd8c18460c53c
JHOHAN <JHOHAN@172.25.4.58>
2018-07-25 11:39:17 -0500

    commit inicial del hola mundo

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -18,6 +18,8 @@ public class HolaMundo {
         // TODO code application logic here
         System.out.println("Hola mundo");
         System.out.println("total");
+        System.out.println("jajajajaja");
+        System.out.println(":)");
     }
     
 }
